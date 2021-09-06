3a9b62657b42b4784e71a071f399e13793c3a761
Atif <atif.taskeen1992@gmail.com>
2021-09-06 20:02:11 +0500

    Completed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,6 @@ import restart from "../../assets/restart.png";
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-
 export default function Header({flagCount, gameOver, showMenu, restartGame}) {
   let [time, setTime] = useState(0);
   const clickMenuHandler = () => {
@@ -25,15 +24,23 @@ export default function Header({flagCount, gameOver, showMenu, restartGame}) {
     }
   }, [time]);
 
+  const reStartHandler = () => {
+    setTime(0);
+    restartGame();
+  }
+
   return (
     <div className="header">
       <div className="header-item">
         <img src={Flag} alt={Flag}/>
         <h4>{flagCount}</h4>
       </div>
-      <div className="header-item">
-        <button onClick={restartGame}><img src={restart} alt={restart}/></button>
-      </div>
+      {
+        gameOver && 
+        <div className="header-item">
+          <button onClick={reStartHandler}><img src={restart} alt={restart}/></button>
+        </div>
+      }
       <div className="header-item">
         <img src={Clock} alt={Clock}/>
         <h4>{time}</h4>
